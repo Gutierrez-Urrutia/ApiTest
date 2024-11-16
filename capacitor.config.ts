eba54af9f9137c184dd747f4c2fd6b1e1547fb93
Pablo Gutiérrez <pa.gutierrezu@duocuc.cl>
2024-11-16 11:48:07 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ApiTest',
+  webDir: 'www'
+};
+
+export default config;
